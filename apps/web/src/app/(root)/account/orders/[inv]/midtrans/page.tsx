ca8e3802d5418dcdba17f350ab1e5f92e89b28a2
RamSekuy <ramaalim34@gmail.com>
2024-07-31 22:09:04 +0700

    feat(order): show error midtrans

--- apps/web/src/app/(root)/account/orders/[inv]/midtrans/page.tsx
+++ apps/web/src/app/(root)/account/orders/[inv]/midtrans/page.tsx
@@ -1,6 +1,7 @@
 import { axiosInstanceSSR } from "@/lib/axios.server-config";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { AxiosError } from "axios";
 
 type Props = {
   params: { inv: string };
@@ -10,9 +11,11 @@ export default async function Page({ params }: Props) {
     .get(`/order/${params.inv}/v1`)
     .then((r) => r.data.data)
     .catch((e) => {
-      console.log(e);
+      if (e instanceof AxiosError) {
+        throw new Error(JSON.stringify(e.response?.data.message));
+      }
       throw new Error(JSON.stringify(e.message));
     });
-  revalidatePath(`/order/${params.inv}`,"page")
+  revalidatePath(`/order/${params.inv}`, "page");
   redirect(paymentLink.payment_url);
 }
